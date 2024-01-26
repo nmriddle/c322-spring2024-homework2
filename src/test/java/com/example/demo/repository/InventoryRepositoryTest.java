25fcb585d8b4a9800ead9a6de7d6e26f17fceb71
Nela Riddle <nelariddle@gmail.com>
2024-01-26 13:36:24 -0500

    added test stuff

---
+++ src/test/java/com/example/demo/repository/InventoryRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryRepositoryTest {
+
+    @Test
+    void addGuitar() {
+        assertTrue(1==1);
+    }
+
+    @Test
+    void getGuitar() {
+    }
+
+    @Test
+    void search() {
+    }
+}
