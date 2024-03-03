63ddcc30a37cad184114e83d844f62642dad4f6f
Nela Riddle <nelariddle@gmail.com>
2024-03-03 15:05:24 -0500

    docker stuff idk

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String greetings(){
-        return "Hey, what's up!";
+        System.out.println("kill me");return "Hey, what's up!";
     }
 }
